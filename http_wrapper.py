9d270a1699a53df1763718eadb80debc517c9a5c
Lucas Bernardes <lucas.bernardes@toradex.com>
2024-08-02 14:13:37 -0300

    Add treatment to unexpected exceptions on requests
    This way, it's possible to treat exceptions that aren't caused by the
    requests, but it's caused by another reason.
    
    Signed-off-by: Lucas Bernardes <lucas.bernardes@toradex.com>

--- http_wrapper.py
+++ http_wrapper.py
@@ -1,7 +1,6 @@
 import logging
 import requests
 import json
-import traceback
 
 logger = logging.getLogger(__name__)
 
@@ -16,7 +15,7 @@ def endpoint_call(url, request_type, headers, body, json):
         elif request_type == "delete":
             res = requests.delete(url, headers=headers)
         else:
-            raise ValueError(f"Unsupported request type: {request_type}")
+            raise ValueError(f"request type {request_type} not supported")
 
         res.raise_for_status()
     except requests.exceptions.HTTPError as errh:
@@ -27,6 +26,8 @@ def endpoint_call(url, request_type, headers, body, json):
         error_message = f"Timeout Error: {errt}"
     except requests.exceptions.RequestException as err:
         error_message = f"Oops: Something Else: {err}"
+    except Exception as e:
+        error_message = f"Unexpected Error: {e}"
     else:
         error_message = None
 
@@ -35,7 +36,6 @@ def endpoint_call(url, request_type, headers, body, json):
             logger.error(json.dumps(res.json(), indent=2))
         except json.JSONDecodeError as e:
             logger.error(f"Error decoding JSON res: {e}")
-            logger.error(traceback.format_exc())
         logger.error(error_message)
         raise Exception(error_message)
 
