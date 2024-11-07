4f8a9e49f45aaa7ed3ddbfccca5f1725bfe26b80
Leonardo Held <leonardo.held@toradex.com>
2024-11-07 12:10:04 -0300

    Solve packaging issue with pid_map.yaml
    This file should be ingested not from the current dir but from the absolute
    path of the Aval container.
    
    This wasn't caught in CI because the workdir from the container will be
    different than the one from using it outside Aval's CI context. Forcing the
    e2e test jobs to NOT clone the repo setting GIT_STRATEGY to 'none' will
    prevent regressions.
    
    Signed-off-by: Leonardo Held <leonardo.held@toradex.com>

--- config_loader.py
+++ config_loader.py
@@ -6,6 +6,6 @@ def load_device_config(device_config_path):
     return toml.load(device_config_path)
 
 
-def load_pid_map(pid_map_path="./pid_map.yaml"):
+def load_pid_map(pid_map_path="/aval/pid_map.yaml"):
     with open(pid_map_path, "r") as f:
         return yaml.safe_load(f)
