c50ed5a8336a93c20f7cd7201d60e9b5af30f56e
Lucas Bernardes <lucas.bernardes@toradex.com>
2025-06-30 10:08:26 -0300

    Remove hardcoded paths for pid_map and main.py
    The previously hardcoded paths only worked when running AVAL inside a
    container, where the code is located at /aval. However, when running AVAL in a
    regular shell, those paths become invalid as they incorrectly point to the
    hardcoded "/aval" location, which may not reflect the actual path where AVAL
    is located.
    
    This commit replaces the hardcoded paths with relative paths, allowing AVAL to
    work both inside containers and when executed from a shell.
    
    It was necessary to add a "cd /aval" before executing AVAL's entrypoint because
    GitLab CI clones the repository inside the container at the "/builds/..." path
    and sets that directory as the job's default WORKDIR. This overrides the
    WORKDIR defined in the Dockerfile of the image specified in "image:".
    
    Related-to: TIE-1042
    
    Signed-off-by: Lucas Bernardes <lucas.bernardes@toradex.com>

--- config_loader.py
+++ config_loader.py
@@ -8,7 +8,7 @@ def load_device_config(device_config_path):
 
 def load_pid_map(pid_map_path=None):
     if not pid_map_path:
-        pid_map_path = "/aval/pid_map.yaml"
+        pid_map_path = "./pid_map.yaml"
 
     with open(pid_map_path, "r") as f:
         return yaml.safe_load(f)
--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ eval $(ssh-agent -s)
 echo "$PRIVATE_KEY" | tr -d '\r' | ssh-add -
 mkdir -p ~/.ssh
 chmod 700 ~/.ssh
-python /aval/main.py "$@"
+python ./main.py "$@"
