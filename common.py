9930a3caf97d5a8a8bbf36a47cdaa312bde009f1
Lucas Bernardes <lucas.bernardes@toradex.com>
2026-02-05 18:34:33 -0300

    common: update "parse_hardware_id()" for luna-sl1680
    The "device_id" pattern for Common Torizon machines has changed as of
    luna-sl1680; it now prefixes the machine name with "torizon-". This commit
    updates the "parse_hardware_id()" function to follow this new pattern.
    
    Signed-off-by: Lucas Bernardes <lucas.bernardes@toradex.com>

--- common.py
+++ common.py
@@ -15,10 +15,10 @@ def parse_hardware_id(device_id):
         return parts[0] + "-" + parts[1] + "-" + parts[2]
     elif "imx93frdm" in device_id:
         return parts[0]
-    elif "torizon-sl1680" in device_id:
-        return parts[1]
     elif "torizon-x86" in device_id:
         return "intel-corei7-64"
+    elif "torizon" in device_id:
+        return parts[1] + "-" + parts[2]
     else:
         return parts[0] + "-" + parts[1]
 
